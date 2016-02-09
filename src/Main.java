a1081ac508bdf9602322331fcb524ff47b11ced7
Roms11 <romaricm@wanadoo.fr>
2016-02-09 12:03:01 +0100

    Graphics

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import Graphics.*;
+
+public class Main {
+	
+	static Window game;
+
+	public static void main(String[] args) {
+		
+		//Créer la fenetre
+		game = new Window();
+		
+		
+		Image image = new Image();
+		game.add(image);
+		
+		Text text = new Text();
+		game.add(text);
+				
+		
+	}
+
+}
