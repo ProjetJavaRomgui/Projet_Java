bf3f504cb807710b3f5ad258cfb8dc32aa8ed7d1
Roms11 <romaricm@wanadoo.fr>
2016-02-12 17:01:03 +0100

    AA

--- src/core/AntsVsSomeBees.java
+++ src/core/AntsVsSomeBees.java
@@ -6,8 +6,8 @@ package core;
 public class AntsVsSomeBees {
 
 	public static void main (String[] args) {
-		AntColony colony = new AntColony(5, 8, 0, 200,10); // specify the colony ]tunnels, length, moats, food, life]
+		AntColony colony = new AntColony(5, 8, 0, 200, 10); // specify the colony ]tunnels, length, moats, food, life]
 		Hive hive = Hive.makeInsaneHive(); // specify the attackers (the hive)
-		new AntGame(colony, hive); // launch the game
+		new AntGame(colony, hive); // launch the game 
 	}
 }
